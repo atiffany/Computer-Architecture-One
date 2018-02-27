e358c89c4db42207c821d65b2594b46f61a51ebd
atiffany <abby.tiffany@gmail.com>
2018-02-26 16:50:16 -0800

    program working as expected

--- project/ls8/ram.js
+++ project/ls8/ram.js
@@ -11,8 +11,10 @@ class RAM {
      * Write (store) MDR value at address MAR
      */
     write(MAR, MDR) {
+        // memory data register(what) and memory address register (where)
         // !!! IMPLEMENT ME
         // write the value in the MDR to the address MAR
+        this.mem[MAR] = MDR;
     }
 
     /**
@@ -23,6 +25,7 @@ class RAM {
     read(MAR) {
         // !!! IMPLEMENT ME
         // Read the value in address MAR and return it
+        return this.mem[MAR];
     }
 }
 
